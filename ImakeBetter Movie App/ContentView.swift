1bbcddc8782aefebf7ddc5552da2824efa1de8cc
kwazzar <fugitiwe@gmail.com>
2023-11-15 19:17:34 +0200

    Initial Commit

---
+++ ImakeBetter Movie App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImakeBetter Movie App
+//
+//  Created by Quasar on 15.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImakeBetter Movie App/ImakeBetter_Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImakeBetter_Movie_AppApp.swift
+//  ImakeBetter Movie App
+//
+//  Created by Quasar on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImakeBetter_Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
